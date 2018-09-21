c79ae6777c27534eddb445f9357f135446654b97
ParkJHyun <xoxc4565@naver.com>
2018-09-21 12:00:02 +0900

    ROS_Gazebo

---
+++ korus_plugin/build/CMakeFiles/control_plugin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/control_plugin.dir/control_plugin.cc.o"
+  "libcontrol_plugin.pdb"
+  "libcontrol_plugin.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/control_plugin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ korus_plugin/build/CMakeFiles/korus_plugin.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/korus_plugin.dir/control_plugin.cc.o"
+  "libkorus_plugin.pdb"
+  "libkorus_plugin.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/korus_plugin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
